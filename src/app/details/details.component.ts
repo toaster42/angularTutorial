2878aa94ac2533d6ffc744720b2bd588ea6e18a4
toaster42 <mike.troester@gmail.com>
2023-11-08 20:43:51 -0800

    display the id value passed using the route

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   template: `
     <p>
-      details works!
+      details works! {{ housingLocationId }}
     </p>
   `,
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
 
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+      this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
